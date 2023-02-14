cbe5889738a1e7fff772723592f28bbaeba033a0
Alain Hsu <alain.hsu@outlook.com>
2023-02-14 11:10:48 +0800

    Initial Commit

---
+++ SwiftUI-Memorize/SwiftUI_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MemorizeApp.swift
+//  SwiftUI-Memorize
+//
+//  Created by Alain Hsu on 2023/2/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
