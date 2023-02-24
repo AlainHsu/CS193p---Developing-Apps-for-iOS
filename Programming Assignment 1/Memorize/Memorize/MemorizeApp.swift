cf000bea5f17b90cf43ca2fd4bbd39e57b2f3bc4
Alain Hsu <alain.hsu@outlook.com>
2023-02-24 14:59:15 +0800

    feat: create project for assignment 1

---
+++ Programming Assignment 1/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alain Hsu on 2023/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
