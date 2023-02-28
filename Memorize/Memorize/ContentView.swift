25c1470f86687ff4c483ca4838497e8734fc76ec
Alain Hsu <alain.hsu@outlook.com>
2023-02-28 15:55:38 +0800

    refactor: refactor view structure

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -13,8 +13,6 @@ struct ContentView: View {
     @State var emojiCount = 12
     var body: some View {
         VStack {
-            Title()
-            Spacer()
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))], content: {
                     ForEach(0 ..< emojiCount, id: \.self) { index in
@@ -24,29 +22,33 @@ struct ContentView: View {
             }
             Spacer()
             HStack {
-                remove().onTapGesture {
-                    if emojiCount > 0 {
-                        emojiCount -= 1
-                    }
-                }
+                remove
                 Spacer()
-                add().onTapGesture {
-                    if emojiCount < emojis.count {
-                        emojiCount += 1
-                    }
-                }
+                add
             }.padding()
 
         }.padding()
             .foregroundColor(.blue)
     }
-}
 
-struct Title: View {
-    var body: some View {
-        Text("Memorize!")
-            .font(.largeTitle)
-            .foregroundColor(.black)
+    var remove: some View {
+        Button(action: {
+            if emojiCount > 0 {
+                emojiCount -= 1
+            }
+        }) {
+            Image(systemName: "minus.circle").font(.largeTitle)
+        }
+    }
+
+    var add: some View {
+        Button(action: {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        }) {
+            Image(systemName: "plus.circle").font(.largeTitle)
+        }
     }
 }
 
@@ -68,45 +70,6 @@ struct CardView: View {
         .onTapGesture {
             isFaceUp = !isFaceUp
         }
-    }
-}
-
-struct AnimalTheme: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "hare").font(.largeTitle)
-            Text("Animals")
-        }
-    }
-}
-
-struct FoodTheme: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "carrot").font(.largeTitle)
-            Text("Animals")
-        }
-    }
-}
-
-struct ActivityTheme: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "soccerball").font(.largeTitle)
-            Text("Animals")
-        }
-    }
-}
-
-struct remove: View {
-    var body: some View {
-        Image(systemName: "minus.circle").font(.largeTitle)
-    }
-}
-
-struct add: View {
-    var body: some View {
-        Image(systemName: "plus.circle").font(.largeTitle)
     }
 }
 
